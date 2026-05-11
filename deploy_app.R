7e2ef7d3488b37c38ed2f4333fdde85c3812dbdd
jeremyash <jeremyd.ash@gmail.com>
2026-05-11 17:54:45 -0400

    initial commit

---
+++ deploy_app.R
@@ -0,0 +1,34 @@
+all_files <- list.files(
+  ".",
+  all.files = TRUE,
+  recursive = TRUE,
+  no.. = TRUE
+)
+
+exclude <- grepl(
+  paste(
+    c(
+      "^\\.git/",
+      "^\\.Rproj\\.user/",
+      "^rsconnect/",
+      "^cache/",
+      "^ndfd_region8/",
+      "^\\.DS_Store$",
+      "\\.grib2$",
+      "\\.bin$",
+      "\\.tif$",
+      "\\.zip$"
+    ),
+    collapse = "|"
+  ),
+  all_files
+)
+
+deploy_files <- all_files[!exclude]
+
+deploy_files[grepl("cache|ndfd_region8|grib2|bin", deploy_files)]
+
+rsconnect::deployApp(
+  appDir = ".",
+  appFiles = deploy_files
+)
